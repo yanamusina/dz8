650169f6d6556052da15eb19bbe221058bc001fb
яна <musina.yana02@yandex.ru>
2023-07-16 15:50:14 +0500

    commit

---
+++ task 54/Program.cs
@@ -0,0 +1,76 @@
+﻿// Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// В итоге получается вот такой массив:
+// 7 4 2 1
+// 9 5 3 2
+// 8 4 4 2
+
+int ReadInt(string text)
+{
+    System.Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+
+int[,] FillMatrix(int row, int col, int leftRange, int rightRange)
+{
+    int[,] tempMatrix = new int[row, col];
+    Random rand = new Random();
+
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < col; j++)
+        {
+            tempMatrix[i, j] = rand.Next(leftRange, rightRange);
+        }
+    }
+    return tempMatrix;
+}
+
+
+void PrintMatrix(int[,] matrixToPrint)
+{
+    for (int i = 0; i < matrixToPrint.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrixToPrint.GetLength(1); j++)
+        {
+            System.Console.Write(matrixToPrint[i, j] + "\t");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+
+int[,] LineMatrix(int[,] matrix, int row, int col)
+{
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < col; j++)
+        {
+            for (int x = 0; x < col - j - 1; x++)
+            {
+                if (matrix[i, x] < matrix[i, x + 1])
+                {
+                    int temp = 0;
+                    temp = matrix[i, x];
+                    matrix[i, x] = matrix[i, x + 1];
+                    matrix[i, x + 1] = temp;
+                }
+            }
+        }
+    }
+
+    return matrix;
+}
+
+int row = ReadInt("Введите количество строк: ");
+int col = ReadInt("Введите количество столбцов: ");
+int[,] matrix = FillMatrix(row, col, 0, 10);
+System.Console.WriteLine(" Начальная матрица:");
+PrintMatrix(matrix);
+System.Console.WriteLine();
+System.Console.WriteLine("Матрица  по убыванию:");
+PrintMatrix(LineMatrix(matrix, row, col));
---
+++ task56/Program.cs
@@ -0,0 +1,89 @@
+﻿// Задача 56: Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 5 2 6 7
+// Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка
+
+int ReadInt(string text)
+{
+    System.Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+
+int[,] FillMatrix(int row, int col, int leftRange, int rightRange)
+{
+    int[,] tempMatrix = new int[row, col];
+    Random rand = new Random();
+
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < col; j++)
+        {
+            tempMatrix[i, j] = rand.Next(leftRange, rightRange);
+        }
+    }
+    return tempMatrix;
+}
+
+
+void PrintMatrix(int[,] matrixToPrint)
+{
+    for (int i = 0; i < matrixToPrint.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrixToPrint.GetLength(1); j++)
+        {
+            System.Console.Write(matrixToPrint[i, j] + "\t");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+
+int[] Sum(int[,] matrix)
+{
+    int[] number = new int[matrix.GetLength(0)];
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            number[i] += matrix[i, j];
+        }
+    }
+    return number;
+}
+
+int Min(int[] array)
+{
+    int min = array[0];
+    int index = 0;
+    for (int i = 1; i <array.Length; i++)
+    {
+        if (min> array[i])
+        {
+            min = array[i];
+            index = i;
+        }
+    }
+    return index;
+}
+
+
+void PrintArray(int[] array)
+{
+    System.Console.WriteLine($"Сумма элементов каждой строки матрицы: [ {string.Join(", ", array)} ]");
+}
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+int row = ReadInt("Введите количество строк: ");
+int col = ReadInt("Введите количество столбцов: ");
+int[,] matrix = FillMatrix(row, col, 0, 10);
+PrintMatrix(matrix);
+System.Console.WriteLine();
+int[] array = Sum(matrix);
+PrintArray(array);
+System.Console.WriteLine();
+System.Console.WriteLine($"Минимальная сумма элементов  {Min(array) + 1} строке");
